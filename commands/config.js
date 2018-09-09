7f2aa09602a9d45be4428e34efa157f674d62ff4
Wheezette <41962633+Wheezette@users.noreply.github.com>
2018-09-09 11:53:00 +0200

    Update config.js

--- commands/config.js
+++ commands/config.js
@@ -12,7 +12,7 @@
 // const [action, key, ...value] = args;
 const Command = require("../base/Command.js");
 
-class Set extends Command {
+class Config extends Command {
   constructor(client) {
     super(client, {
       name: "config",
@@ -36,51 +36,51 @@ class Set extends Command {
     // Secondly, if a user does `-set edit <key> <new value>`, let's change it
     if (action === "edit") {
       // User must specify a key.
-      if (!key) return message.reply("Please specify a key to edit");
+      if (!key) return message.channel.send("Musisz podać klucz, który chcesz edytować.");
       // User must specify a key that actually exists!
-      if (!settings[key]) return message.reply("This key does not exist in the settings");
+      if (!settings[key]) return message.channel.send("Podany przez Ciebie klucz nie istnieje.");
       // User must specify a value to change.
       const joinedValue = value.join(" ");
-      if (joinedValue.length < 1) return message.reply("Please specify a new value");
+      if (joinedValue.length < 1) return message.channel.send("Podaj wartość, która ma zostać ustawiona.");
       // User must specify a different value than the current one.
-      if (joinedValue === settings[key]) return message.reply("This setting already has that value!");
+      if (joinedValue === settings[key]) return message.channel.send("To ustawienie zawiera już wartość, którą podałeś(aś).");
 
       // If the guild does not have any overrides, initialize it.
       if (!this.client.settings.has(message.guild.id)) this.client.settings.set(message.guild.id, {});
 
       // Modify the guild overrides directly.
       this.client.settings.set(message.guild.id, joinedValue, key);
-      message.reply(`${key} successfully edited to ${joinedValue}`);
+      message.channel.send(`Klucz **${key}** został edytowany na **${joinedValue}**.`);
     } else
   
     // If a user does `-set del <key>`, let's ask the user if they're sure...
     if (action === "del" || action === "reset") {
-      if (!key) return message.reply("Please specify a key to delete (reset).");
-      if (!settings[key]) return message.reply("This key does not exist in the settings");
-      if (!overrides[key]) return message.reply("This key does not have an override and is already using defaults.");
+      if (!key) return message.channel.send("Musisz podać klucz, który chcesz zresetować do domyślnej wartości.");
+      if (!settings[key]) return message.channel.send("Podany przez Ciebie klucz nie istnieje.");
+      if (!overrides[key]) return message.channel.send("This key does not have an override and is already using defaults.");
 
       // Throw the 'are you sure?' text at them.
-      const response = await this.client.awaitReply(message, `Are you sure you want to reset \`${key}\` to the default \`${defaults[key]}\`?`);
+      const response = await this.client.awaitReply(message, `Czy napewno chcesz zresetować klucz \`${key}\` do domyślnej wartości \`${defaults[key]}\`? Wpisz pod tą wiadomością **tak** jeśli chcesz zresetować, lub **anuluj** jeśli chcesz anulować.`);
 
       // If they respond with y or yes, continue.
-      if (["y", "yes"].includes(response)) {
+      if (["y", "yes", "tak"].includes(response)) {
 
         // We reset the `key` here.
         this.client.settings.delete(message.guild.id, key);
-        message.reply(`${key} was successfully reset to default.`);
+        message.channel.send(`Klucz **${key}** został zresetowany do domyślnej wartości.`);
       } else
 
       // If they respond with n or no, we inform them that the action has been cancelled.
-      if (["n","no","cancel"].includes(response)) {
-        message.reply(`Your setting for \`${key}\` remains at \`${settings[key]}\``);
+      if (["n","no","cancel", "anuluj", "nie"].includes(response)) {
+        message.channel.send(`Klucz \`${key}\` pozostał na \`${settings[key]}\``);
       }
     } else
   
     // Using `-set get <key>` we simply return the current value for the guild.
     if (action === "get") {
-      if (!key) return message.reply("Please specify a key to view");
-      if (!settings[key]) return message.reply("This key does not exist in the settings");
-      message.reply(`The value of ${key} is currently ${settings[key]}`);
+      if (!key) return message.channel.send("Musisz podać klucz, który chcesz zobaczyć.");
+      if (!settings[key]) return message.reply("Podany przez Ciebie klucz nie istnieje.");
+      message.reply(`Wartość klucza **${key}** jest ustawiona na **${settings[key]}**`);
       
     } else {
       // Otherwise, the default action is to return the whole configuration;
@@ -88,9 +88,9 @@ class Set extends Command {
       Object.entries(settings).forEach(([key, value]) => {
         array.push(`${key}${" ".repeat(20 - key.length)}::  ${value}`); 
       });
-      await message.channel.send(`= Current Guild Settings =\n${array.join("\n")}`, {code: "asciidoc"});
+      await message.channel.send(`= Konfiguracja serwera =\n${array.join("\n")}`, {code: "asciidoc"});
     }
   }
 }
 
-module.exports = Set;
+module.exports = Config;
