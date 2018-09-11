27150617ec727d27565c7f49adc110741742e2f1
Wheezette <41962633+Wheezette@users.noreply.github.com>
2018-09-11 09:39:42 +0200

    Create admins.js

---
+++ commands/admins.js
@@ -0,0 +1,27 @@
+const Command = require("../base/Command.js");
+const Discord = require("discord.js");
+
+class Admins extends Command {
+  constructor(client) {
+    super(client, {
+      name: "admins",
+      description: "Wyswietla administracje bota.",
+      usage: "ping",
+      aliases: ["globalsupport"]
+    });
+  }
+
+  async run(message, args, level) { // eslint-disable-line no-unused-vars
+    const embed = new Discord.RichEmbed()
+    .setAuthor("Administracja bota")
+    .addField("Developer / Twóca CookieBOT?a", "xCookieTM#9613")
+    .addField("Vice Developer", "*Brak użytkownikow w tej kategorii*")
+    .addField("Global Support", "*Brak uzytkownikow w tej kategorii*")
+    message.channel.send(embed);
+  }
+
+}
+
+
+
+module.exports = Admins;
