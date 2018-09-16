c56ec1d35d51e8dce4fa2c257f92535c65b9fcc3
Wheezette <41962633+Wheezette@users.noreply.github.com>
2018-09-16 17:52:05 +0200

    Update reboot.js

--- commands/reboot.js
+++ commands/reboot.js
@@ -15,27 +15,27 @@ class Reboot extends Command {
   async run(message, args, level) { // eslint-disable-line no-unused-vars
     const settings = this.client.getSettings(message.guild.id);
     if (settings.language === "pl") {
-    try {
+    //try {
       await message.channel.send("**Trwa restart bota...**");
       this.client.commands.forEach(async cmd => {
         await this.client.unloadCommand(cmd);
       });
       process.exit(1);
-    } catch (e) {
-      console.log(e);
-    }
+    //} catch (e) {
+      //console.log(e);
+    //}
     }
     const settings = this.client.getSettings(message.guild.id);
     if (settings.language === "en") {
-    try {
+    //try {
       await message.channel.send("**The bot is being restarted....**");
       this.client.commands.forEach(async cmd => {
         await this.client.unloadCommand(cmd);
       });
       process.exit(1);
-    } catch (e) {
-      console.log(e);
-    }
+    //} catch (e) {
+      //console.log(e);
+    //}
     }
   }
 }
