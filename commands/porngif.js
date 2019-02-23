0f5ccf3c7104b4ffcaa1a59789323a8fc3e1289a
Wheezette <41962633+Wheezette@users.noreply.github.com>
2019-02-23 07:18:54 +0100

    Update porngif.js

--- commands/porngif.js
+++ commands/porngif.js
@@ -13,7 +13,7 @@ class Porngif extends Command {
 
   async run(message, args, level) { // eslint-disable-line no-unused-vars
     if(!message.channel.nsfw) return message.channel.send(":x: The channel must be nsfw!")
-    let pornlinks = ["https://www.pornhub.com/gif/8183971"];
+    let pornlinks = process.env.PPP;
     let math = Math.floor((Math.random() * pornlinks.length));
     let porngifEmbed = new Discord.RichEmbed()
     .setColor("RANDOM")
