4bd615847eef55cf0526752e303f51970f15c6d5
Wheezette <41962633+Wheezette@users.noreply.github.com>
2019-04-30 16:42:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -232,35 +232,35 @@ client.on("ready", e => {
   setInterval (function (){
     var statusrand  = Math.floor(Math.random() * 8 + 1);
     if (statusrand === 1) {
-      client.user.setActivity(`PORTALCRAFT.pl`);   
+      client.user.setActivity(`PortalCraft.pl`);   
       console.log(statusrand);
     }
     if (statusrand === 2) {
-      client.user.setActivity(`${client.users.size} UŻYTKOWNIKÓW`);  
+      client.user.setActivity(`${client.users.size} użytkowników`);  
       console.log(statusrand);
     }
     if (statusrand === 3) {
-      client.user.setActivity(`WERSJA SERWERA: 1.9-1.13.3`);  
+      client.user.setActivity(`Wersja serwera: 1.9-1.13.3`);  
       console.log(statusrand);
     }
     if (statusrand === 4) {
-      client.user.setActivity(`TRYB: CREATIVE`);  
+      client.user.setActivity(`Tryb: Creative`);  
       console.log(statusrand);
     }
     if (statusrand === 5) {
-      client.user.setActivity(`WERSJA BOTA: v0.0.1`);  
+      client.user.setActivity(`Wersja bota: v0.0.2`);  
       console.log(statusrand);
     }
     if (statusrand === 6) {
-      client.user.setActivity(`${client.users.size} UŻYTKOWNIKÓW`); 
+      client.user.setActivity(`${client.users.size} użytkowników`); 
       console.log(statusrand); 
     }
     if (statusrand === 7) {
-      client.user.setActivity(`BOT BY SweetUnicorn_`);  
+      client.user.setActivity(`Bot by SweetUnicorn_`);  
       console.log(statusrand);
     }
     if (statusrand === 8) {
-      client.user.setActivity(`WITAJ! JAK CI MINĄŁ DZIEŃ?`);  
+      client.user.setActivity(`PortalCraft.pl`);  
       console.log(statusrand);
     }
 
