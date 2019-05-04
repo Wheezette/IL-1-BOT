26e41744206653f1b8ac33b0e6c3561e48dd229f
Wheezette <41962633+Wheezette@users.noreply.github.com>
2019-05-04 22:23:08 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,20 +1,37 @@
 const Command = require("../base/Command.js");
 const Discord = require("discord.js");
 
-class Invite extends Command {
+class Hug extends Command {
   constructor(client) {
     super(client, {
-      name: "invite",
-      description: "Link do serwera pomocy i zaproszenia bota.",
-      usage: "invite",
-      aliases: ["inv"]
+      name: "hug",
+      description: "Przytula podanego użytkownika.",
+      usage: "hug <@member>",
+      aliases: ["przytul"]
     });
   }
 
   async run(message, args, level) { // eslint-disable-line no-unused-vars
-    const embed = new Discord.RichEmbed()
-    .setDescription("Aby dołączyć na serwer bota >> [KLIK](https://discord.gg/WTekf4a) << (dowiesz się tam o aktualizacjach oraz otrzymasz pomoc z botem) \n \nAby zaprosić bota na swój serwer >> [KLIK](https://discordapp.com/api/oauth2/authorize?client_id=458569537286176768&permissions=842394839&scope=bot) <<");
-     message.channel.send(embed);
+    //const embed = new Discord.RichEmbed()
+    //.setDescription("Aby dołączyć na serwer bota >> [KLIK](https://discord.gg/WTekf4a) << (dowiesz się tam o aktualizacjach oraz otrzymasz pomoc z botem) \n \nAby zaprosić bota na swój serwer >> [KLIK](https://discordapp.com/api/oauth2/authorize?client_id=458569537286176768&permissions=842394839&scope=bot) <<");
+     //message.channel.send(embed);
+    let aUser = message.mentions.users.first(); //|| message.author || message.user.id;
+    let huglinks = ["https://media.giphy.com/media/l0HlOvJ7yaacpuSas/giphy.gif", "https://media.giphy.com/media/xT39CXg70nNS0MFNLy/giphy.gif", "https://media.giphy.com/media/143v0Z4767T15e/giphy.gif", "https://media.giphy.com/media/BVRoqYseaRdn2/giphy.gif", "https://media.giphy.com/media/od5H3PmEG5EVq/giphy.gif"];
+    let math = Math.floor((Math.random() * huglinks.length));
+    let hugEmbed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setAuthor(`Użytkownik ${message.author.tag} przytulił(a) ${aUser.tag}.`, 'https://cdn.discordapp.com/emojis/472468044871106591.png?v=1')
+    .setImage(huglinks[math])
+    let hugMe = new Discord.RichEmbed()
+    .setColor("f083fa")
+    .setAuthor(`${message.author.tag} przytulił(a) samego/samą siebie.`)
+    .setImage(huglinks[math])
+    if(args[0] == message.author) return message.channel.send()
+    if(!args[0]) return message.channel.send(hugMe);
+
+    message.channel.send(hugEmbed);
+
+    
   }
 }
 
