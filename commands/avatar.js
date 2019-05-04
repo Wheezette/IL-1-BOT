d5674760a3470b798c744013241503ec1f7dd245
Wheezette <41962633+Wheezette@users.noreply.github.com>
2019-05-04 21:55:52 +0200

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -30,6 +30,12 @@ class Avatar extends Command {
     //.addField("Gra w:", `${aUser.presence.game ? aUser.presence.game.name : 'nic'}`)
     //.setFooter(`${moment.utc(message.createdAt).format('HH:mm:ss')} | Vextie by Nastti#5705`)
     //message.channel.send(userinfo);
+    let errorMessage = new Discord.RichEmbed()
+    .setColor("d42d2d")
+    .setAuthor("Wystąpił Błąd", "https://www.google.pl/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiSwtPu04LiAhVxposKHWw8Cq4QjRx6BAgBEAU&url=https%3A%2F%2Fwww.imagenesmy.com%2Fimagenes%2Ferror-icon-d7.html&psig=AOvVaw3ZrGvMtXQMvqLsWM32pW9P&ust=1557086082110444")
+    .setDescription("Musisz podać osobę do wyświetlenia avatara.")
+    .setImage("https://www.google.pl/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiT0Ny804LiAhVqpIsKHae6DRgQjRx6BAgBEAU&url=https%3A%2F%2Fwallpapersafari.com%2Fcomputer-error-wallpaper%2F&psig=AOvVaw3l34SZInBXfJ38wU9DKrVh&ust=1557085980008482")
+    if(!args[0]) return message.channel.send(errorMessage);
     message.channel.send(avatarEmbed);
   }
 }
