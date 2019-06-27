4f2b1a9b2c4f547422678ee087c6d04ceea4e498
Wheezette <41962633+Wheezette@users.noreply.github.com>
2019-06-27 23:08:52 +0200

    Create clear.js

---
+++ commands/clear.js
@@ -0,0 +1,22 @@
+const Command = require("../base/Command.js");
+
+class Clear extends Command {
+  constructor(client) {
+    super(client, {
+      name: "clear",
+      description: "Wysyła wiadomość o podanej treści na kanał.",
+      usage: "clear <ilość>",
+      aliases: ["purge"]
+    });
+  }
+
+  async run(message, args, level) { // eslint-disable-line no-unused-var
+    let messagecount = parseInt(args.join(' '));
+    message.channel.fetchMessages({
+      limit: messagecount
+    }).then(messages => message.channel.bulkDelete(messages));
+    let purgeSuccessMessage = await message.channel.send(`Zostało wyczyszczone **${messagecount}** wiadomości z tego kanału!`);
+    purgeSuccessMessage.delete(10000);
+  }
+}
+module.exports = Clear;
