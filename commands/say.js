f804b93f1c3ca9f34fce06dd8163e12678f5430e
Wheezette <41962633+Wheezette@users.noreply.github.com>
2019-06-27 22:27:55 +0200

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,18 @@
+const Command = require("../base/Command.js");
+
+class Say extends Command {
+  constructor(client) {
+    super(client, {
+      name: "say",
+      description: "Wysyła wiadomość o podanej treści na kanał.",
+      usage: "say <wiadomość>",
+      aliases: ["sayy"]
+    });
+  }
+
+  async run(message, args, level) { // eslint-disable-line no-unused-var
+    let sayMessage = args.join(" ");
+    message.delete();
+    message.channel.send(sayMessage);
+  }
+module.exports = Say;
