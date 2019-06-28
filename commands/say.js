d27f7843d982d4a2dc1e033f58bd51df3ca55fb7
Wheezette <41962633+Wheezette@users.noreply.github.com>
2019-06-28 19:14:56 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,4 +1,5 @@
 const Command = require("../base/Command.js");
+let Discord = require("discord.js");
 
 class Say extends Command {
   constructor(client) {
@@ -12,8 +13,18 @@ class Say extends Command {
 
   async run(message, args, level) { // eslint-disable-line no-unused-var
     //let sayMessage = args.join(" ");
-    message.delete();
-    message.channel.send(args.join(" "));
+    let errEmb = new Discord.RichEmbed()
+    .setColor("bc3b3b")
+    .setAuthor("Wystąpił Błąd")
+    .setDescription("Wymagane jest podanie wiadomości, którą chcesz wysłać.\n**Poprawne użycie:** `!say <wiadomość>`.")
+    .setFooter(`Jeśli błąd nie ustąpi napisz do programisty bota.`);
+    
+    if(!args[0]) {
+      return message.channel.send(errEmb);
+    } else {
+      message.delete();
+      message.channel.send(args.join(" "));
+    }
   }
 }
 module.exports = Say;
