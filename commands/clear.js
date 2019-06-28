5e2229636512738d67deba979c2de684ca14ae86
Wheezette <41962633+Wheezette@users.noreply.github.com>
2019-06-28 18:46:00 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -11,14 +11,17 @@ class Clear extends Command {
   }
 
   async run(message, args, level) { // eslint-disable-line no-unused-var
-    let messagecount = parseInt(args[0]);
-    if (isNaN(messagecount)) {
-        return message.reply('Ilość wiadomości do wyczyszczenia musi być cyfrą.');
-    }
-    if (messagecount <= 0 || messagecount > 100) {
-      return message.channel.send("Ilość wiadomości do wyczyszczenia musi być cyfrą większą od zera i niższą od 100.")'
-    }
-    if (messagecount > 0 || messagecount < 100) { message.channel.fetchMessages({
+    let messagecount = parseInt(args.join(" "));
+    
+    if(!args[0]) return message.channel.send("Musisz podać ilość wiadomości do wyczyszczenia.")
+      
+    //if (isNaN(messagecount)) {
+        //return message.reply('Ilość wiadomości do wyczyszczenia musi być cyfrą.');
+    //}
+    //if (messagecount <= 0 || messagecount > 100) {
+      //return message.channel.send("Ilość wiadomości do wyczyszczenia musi być cyfrą większą od zera i niższą od 100.")'
+    //}
+    if(args[0]) return message.channel.fetchMessages({
       limit: messagecount
     }).then(messages => message.channel.bulkDelete(messages))};
     let purgeSuccessMessage = await message.channel.send(`Zostało wyczyszczone **${messagecount}** wiadomości z tego kanału!`);
